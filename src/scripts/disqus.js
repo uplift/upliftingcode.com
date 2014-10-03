3ff3f12f6244ca1e9f36da6eaa0442ff9b33511e
Uplift <git@upliftingcode.com>
2014-10-03 11:05:38 +0100

    Add missing scripts directory

---
+++ src/scripts/disqus.js
@@ -0,0 +1,2 @@
+var disqus_shortname = 'upliftingcode'/*,disqus_developer = 1*/;
+(function(d){var s = d.createElement('script'); s.async=1;s.src='http://' + disqus_shortname + '.disqus.com/count.js';d.getElementsByTagName('BODY')[0].appendChild(s);}(document));
